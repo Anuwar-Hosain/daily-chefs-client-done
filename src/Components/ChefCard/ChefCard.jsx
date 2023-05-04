84793385f22c9b41772fe6f009f8d49014255f09
Anuwar-Hosain <anuwarhossn@gmail.com>
2023-05-04 23:22:22 +0600

    chef section done

--- src/Components/ChefCard/ChefCard.jsx
+++ src/Components/ChefCard/ChefCard.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./ChefCard.css";
+import { BsFillHandThumbsUpFill, BsArrowRight } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const ChefCard = (props) => {
   const {
@@ -9,12 +11,27 @@ const ChefCard = (props) => {
     years_of_experience,
     number_of_recipes,
     likes,
+    title,
   } = props.chef;
   console.log(props.chef);
   return (
     <div className="chef-card">
       <img src={chef_picture} alt="" className="chef-img" />
-      <h1>ChefCard</h1>
+      <div className="ex-re-li">
+        <p className="chef-ex">Experience: {years_of_experience}</p>
+        <p className="chef-re">Number of Recipes: {number_of_recipes}</p>
+        <p className="chef-li">
+          <BsFillHandThumbsUpFill className="like"></BsFillHandThumbsUpFill>
+          {likes}
+        </p>
+      </div>
+      <h1 className="chef-title">{title}</h1>
+      <h1 className="chef-name">Name:{chef_name}</h1>
+      <div className="btn-view-chef">
+        <Link to="/" className="chef-btn">
+          View Recipe <BsArrowRight></BsArrowRight>
+        </Link>
+      </div>
     </div>
   );
 };
